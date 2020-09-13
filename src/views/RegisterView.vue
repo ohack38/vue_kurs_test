90a65174173d41a7e2e0f50a23dbb1d931fa949b
Rasmus <64076269+racemaus@users.noreply.github.com>
2020-09-13 16:01:41 +0300

    register styling and fixes

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -4,13 +4,13 @@
     <input type="text" name="username" v-model="input.username" placeholder="Användarnamn" /><br>
     <input type="password" name="password" v-model="input.password" placeholder="Lösenord" /><br>
     <input type="email" name="email" v-model="input.email" placeholder="Epost" /><br>
-    <input type="zip" name="zip" v-model="input.zip" placeholder="Postnummer" /><br>
+    <input type="number" name="zip" v-model="input.zip" placeholder="Postnummer" /><br>
     <button type="button">Registrera</button>
 </div>
 </template>
 <script>
     export default {
-        name: 'Login',
+        name: 'register',
         data() {
             return {
                 input: {
@@ -23,3 +23,54 @@
         }
     }
 </script>
+<style scoped>
+input[type=text]{
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid green;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+input[type=email] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid green;
+    box-sizing: border-box;
+}
+
+input[type=password] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid green;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+input[type=number] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid green;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+button{
+    width: inherit;
+    height: inherit;
+    background-color: #424242;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+button:hover {
+    background-image: linear-gradient(to right, green, lightgreen);
+}
+
+</style>
