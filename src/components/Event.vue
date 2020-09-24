8feeaec059793e832b5082c698e3c97ba705750f
saarishe <70575886+saarishe@users.noreply.github.com>
2020-09-24 12:22:28 +0300

    EventItem och Event component created

---
+++ src/components/Event.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <div v-for="event in events" :key="event.id">
+        <EventItem :event="event" />
+    </div>
+</div>
+    
+</template>
+
+<script>
+
+import EventItem from './items/EventItem'
+
+export default {
+    name: 'Event',
+    props: ['events'],
+    components: {
+        EventItem
+    }
+}
+</script>
---
+++ src/components/items/EventItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-if="event" class="eventtype">
+    <a :href="event.info_url">
+      <h3>{{event.name.fi}}</h3>
+    </a>
+
+    <h5 class="timer">{{event.where_when_duration.where_and_when}}</h5>
+    <div v-html="event.description.body"></div>
+    <button>Join Event</button>
+    <button>Interested</button>
+    <img loading=lazy class="thumbnail" :src="event.description.images[0].url">
+  </div>
+  <p v-else>loading.....</p>
+</template>
+
+<script>
+export default {
+  name: "EventItem",
+  props: ["event"],
+};
+</script>
+
+<style scoped>
+button {
+  background-color: #44a6be;
+  color: white;
+  border-radius: 100px 100px;
+  padding: 10px 20px;
+  margin: 20px;
+  border: none;
+}
+</style>
