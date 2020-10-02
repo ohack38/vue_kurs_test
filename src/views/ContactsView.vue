d108208ba5551a21c6cfbe617acc787fcd6f7b9e
ofversfr <ofverstromfrida@gmail.com>
2020-10-02 16:32:44 +0300

    Innehåll för contacts, settings & wallet

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -1,5 +1,123 @@
 <template>
-  <div id="settings">
-      <h1>My Contacts</h1>
+  <div id="profile">
+
+    <h1 style="font-size: 50px">My Contacts</h1>
+
+    <div id="innerProfile">
+      <img id="profilePicture" src="../assets/profile.png" />
+      <div id="profileInfo">
+        <h2 class="innerInfo">Name</h2> 
+        <h2 class="innerInfo">City</h2>
+        <h2 class="innerInfo">Events #</h2>
+        <h2 class="innerInfo">Activity</h2>
       </div>
-      </template>
+      <div id="profileText">
+          <p class="person"></p>
+          <p class="person>"></p>
+          <p class="person"></p>
+          <p class="person"></p>
+      </div>
+    </div>
+
+    <br>
+
+    <div id="innerProfile">
+      <img id="profilePicture" src="../assets/profile.png" />
+      <div id="profileInfo">
+        <h2 class="innerInfo">Name</h2> 
+        <h2 class="innerInfo">City</h2>
+        <h2 class="innerInfo">Events #</h2>
+        <h2 class="innerInfo">Activity</h2>
+      </div>
+      <div id="profileText">
+          <p class="person"></p>
+          <p class="person>"></p>
+          <p class="person"></p>
+          <p class="person"></p>
+      </div>
+    </div>
+
+    <br>
+
+    <div id="innerProfile">
+      <img id="profilePicture" src="../assets/profile.png" />
+      <div id="profileInfo">
+        <h2 class="innerInfo">Name</h2> 
+        <h2 class="innerInfo">City</h2>
+        <h2 class="innerInfo">Events #</h2>
+        <h2 class="innerInfo">Activity</h2>
+      </div>
+      <div id="profileText">
+          <p class="person"></p>
+          <p class="person>"></p>
+          <p class="person"></p>
+          <p class="person"></p>
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<style scoped>
+button {
+  width: 150px;
+  height: 120px;
+  cursor: pointer;
+  font-size: 45px;
+}
+button:hover {
+  background-image: linear-gradient(to right, salmon, white);
+}
+#profile {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 2%;
+  max-height: 100%;
+}
+#innerProfile {
+  display: flex;
+  flex-direction: row;
+  background-image: linear-gradient(to right, salmon, rgba(255, 255, 255, 0));
+  padding: 10px;
+  border: none;
+  border-radius: 10px 10px;
+}
+.profileText{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+}
+.innerInfo {
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+  margin-left: 10px;
+}
+#profilePicture {
+  max-width: 30%;
+  height: auto;
+  border: none;
+  border-radius: 10px 10px;
+}
+
+#profileLinks {
+  padding-top: 6%;
+  font-size: 2.5vh;
+  justify-content: center;
+  width: 100%;
+  height: 70px;
+}
+.pButton {
+  font-weight: bold;
+  font-style: italic;
+  font-size: 1vw;
+  color: white;
+  height: 100%;
+  background-color: salmon;
+  border-radius: 15px 100px;
+  padding: 10px 17px;
+  margin: 7px;
+}
+</style>
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,5 +1,31 @@
 <template>
   <div id="settings">
+
       <h1>Settings</h1>
-      </div>
-      </template>
+      <button class="pButton" type="button">Edit profile</button>
+
+  </div>
+</template>
+
+      <style>
+button {
+  width: 150px;
+  height: 120px;
+  cursor: pointer;
+  font-size: 45px;
+}
+button:hover {
+  background-image: linear-gradient(to right, salmon, white);
+}
+.pButton {
+  font-weight: bold;
+  font-style: italic;
+  font-size: 1vw;
+  color: white;
+  height: 100%;
+  background-color: salmon;
+  border-radius: 15px 100px;
+  padding: 10px 17px;
+  margin: 7px;
+}
+        </style>
