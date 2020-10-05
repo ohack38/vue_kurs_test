1891ac3782d46b6e4295ea6f4c7ec9d9e22cae70
racemaus <64076269+racemaus@users.noreply.github.com>
2020-10-05 15:19:55 +0300

    localstorage fix + art by default
    -satt till fungerande infinite scroll
    
    -buttons lika stora

--- src/views/EventView.vue
+++ src/views/EventView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="eventItem" infinite-scroll-disabled="busy" infinite-scroll-distance="10" >
+  <div class="eventItem" v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="10" >
     <div class="selectButtons">
       <button id="art" @click="select" type="button">Art</button>
       <button id="culture" @click="select" type="button">Culture</button>
@@ -11,7 +11,7 @@
       
     </div>
     <div class="eventColumn">
-      <Event v-bind:events="events"/> 
+      <Event v-bind:events="events" v-on:add-event="addEvent"/> 
     </div>
 
   </div>
@@ -34,7 +34,8 @@ export default {
       events: [],
       limit: 10,
       myEvents: [],
-      busy: false
+      busy: false,
+      searchID: "art"
     }
 
   },
@@ -58,6 +59,31 @@ export default {
         .then(res => {
           const append = res.data.data.slice(
             this.events.length,
+            this.events.length + this.limit,
+            this.searchID = buttonID
+          );
+          this.events = this.events.concat(append);
+          this.busy = false;
+          this.searchID = buttonID
+          });
+    },
+    addEvent(id, name, description, url){ 
+      alert(id + 'added to localStorage')
+      this.myEvents.push({ id: id, name: name, decription: description, url: url});
+      localStorage.setItem('storage', JSON.stringify(this.myEvents))
+    },
+    loadMore() {
+      // Infinite scroll, laddar in 'art' kategorin allra först med this.searchID
+      // när man klickar på en knapp uppdateras this.searchID till knappens ID
+      // för att fortsätta infinite scroll med rätt kategori 
+
+      this.busy = true;
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      //baseUrl: open-api.myhelsinki.fi/v1/
+      axios.get(`${proxy}http://open-api.myhelsinki.fi/v1/events/?tags_search=`+this.searchID)
+        .then(res => {
+          const append = res.data.data.slice(
+            this.events.length,
             this.events.length + this.limit
           );
           this.events = this.events.concat(append);
@@ -65,11 +91,7 @@ export default {
           });
     }
   },
-  addEvent(id, name, description, url){ 
-      alert(id + 'added to localStorage')
-      this.myEvents.push({ id: id, name: name, decription: description, url: url});
-      localStorage.setItem('storage', JSON.stringify(this.myEvents))
-  }
+  
 }
 </script>
 
@@ -94,14 +116,15 @@ button{
   font-style: italic;
   font-size: 1vw;
   color: white;
-  height: 100%;
+  height: 50px;
+  width: 100px;
   background-color: salmon;
   border-radius: 15px 100px;
   padding: 10px 17px;
   margin: 7px;
 }
 button:hover {
-    background-image: linear-gradient(to right, salmon, white);
+  background-image: linear-gradient(to right, salmon, white);
 }
 .eventItem{
   display: flex;
@@ -118,5 +141,8 @@ button:hover {
 .eventColumn{
   display: flex;
   flex-direction: column;
+}
+.activeButton{
+  background-color: green;
 }
 </style>
