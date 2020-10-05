c33d2565271f38fae516419923078d880552775a
HannesHolmlund <holmlund.hannes@gmail.com>
2020-10-05 21:49:43 +0300

    Added loading svg to activity

--- src/views/ActivityView.vue
+++ src/views/ActivityView.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="hello" v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="10" >
+  <div style="height:100%; vertical-align:center" v-if="loading">
+    <svg id="loading-svg" class="progress-ring" width="240" height="240">
+      
+      <linearGradient id="linear">
+        <stop offset="25%" stop-color="#ffffff" stop-opacity="1"/>
+         <stop offset="50%" stop-color="#ffffff" stop-opacity=".75"/>
+         <stop offset="65%" stop-color="#ffffff" stop-opacity=".5"/>
+         <stop offset="100%" stop-color="#ffffff" stop-opacity="0"/>
+      </linearGradient>
+
+        <circle
+          class="progress-ring_circle"
+          stroke="url(#linear)"
+          stroke-width="7"
+          fill="transparent"
+          r="60"
+          cx="120"
+          cy="120"/>       
+
+        <animateTransform 
+          attributeName="transform" 
+          attributeType="XML" 
+          type="rotate"
+          dur="2s" 
+          from="0 0 0"
+          to="360 0 0" 
+          repeatCount="indefinite" />
+      </svg>
+    </div>
     <Activity v-bind:activities="activities" v-on:add-event="addEvent"/> 
   </div>
 </template>
@@ -19,7 +48,8 @@ export default {
       activities: [],
       limit: 10,
       busy: false,
-      myEvents: []
+      myEvents: [],
+      loading: true
     }
   },
   directives:{
@@ -35,7 +65,8 @@ export default {
         .then(res => {
           const append = res.data.data.slice(
             this.activities.length,
-            this.activities.length + this.limit
+            this.activities.length + this.limit,
+            this.loading = false
           );
           this.activities = this.activities.concat(append);
           this.busy = false;
@@ -76,5 +107,9 @@ li {
 }
 a {
   color: #42b983;
+}
+.progress-ring_circle{
+  stroke-dasharray: 280;
+  stroke-linecap: round;
 }
 </style>
