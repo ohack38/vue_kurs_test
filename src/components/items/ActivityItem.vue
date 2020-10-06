fc3ec92252cc18bf997f2e7689f008b33cb36840
Oscar Hackman <ohackman1@gmail.com>
2020-10-06 17:59:25 +0300

    delete button o toggle between buttons almost färdig

--- src/components/items/ActivityItem.vue
+++ src/components/items/ActivityItem.vue
@@ -15,8 +15,15 @@
         <p>Participate</p>
       </a>
     </button>
-    <!-- toggle buttons to delete interest -->
-    <button @click="$emit('add-event', activity.id, activity.name.fi, activity.where_when_duration.where_and_when, activity.description.body, activity.info_url ); interested = !interested" v-bind:class="interested ? 'clickedColor' : 'defaultColor'">Interested</button>
+
+    <!-- toggle button beroende på interest -->
+    <!-- add interest-->
+    <button @click="$emit('add-event', activity.id, activity.name.fi, activity.where_when_duration.where_and_when, activity.description.body, activity.info_url ); 
+      interested = !interested" v-show="!interested">Interested</button>
+
+    <!-- delete button-->
+    <button @click="$emit('del-event', activity.id); interested = interested" v-show="interested">Not interested?</button>
+    
     <img loading=lazy class="thumbnail" :src="activity.description.images[0].url">
   </div>
   <p v-else>loading.....</p>
