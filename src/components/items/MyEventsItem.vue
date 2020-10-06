1e2c759fd4af0be8f4a5fa47bd62d0bd0b6ce3b7
racemaus <64076269+racemaus@users.noreply.github.com>
2020-10-06 10:51:36 +0300

    myevents update

--- src/components/items/MyEventsItem.vue
+++ src/components/items/MyEventsItem.vue
@@ -3,7 +3,7 @@
         <a :href="eventItem.url">
             <h3>{{eventItem.name}}</h3>
         </a>
-        <h5 class='timer'>{{eventItem.when_where}}</h5>
+        <div v-html="eventItem.when_where"></div>
         <div v-html="eventItem.decription"></div>
     </div>
 </template>
